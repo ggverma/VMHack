10cff78899d7a8c353f76f6fad61c6e202c3c949
Gautam Verma <vermag@vmware.com>
2018-07-19 14:17:12 -0700

    data

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
+
+userInfo = None
+with open('userdata.json') as db:
+    userInfo = json.loads(db)
+
+productInfo = None
+with open('productdata.json') as db:
+    productInfo = json.loads(db)
 
 @app.route('/')
 def index():
@@ -29,7 +38,24 @@ def login():
     #---------------------------------------
     # A web page showing some random products and facebook login
     #---------------------------------------
-    return render_template('index.html', url="http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif")
+    return render_template('login.html')
+
+@app.route('/loginform', methods = ['POST'])
+def loginform():
+    uname = request.form['uname']
+    pwd = request.form['pwd']
+
+    if uname in userInfo:
+        if pwd == db[uname]['pwd']:
+            # Sucess
+            return render_template('home.html')
+        else:
+            # Incorrect pass
+            return render_template('login.html')
+    else:
+        # Unregistered user
+        return render_template('login.html')
+
 
 def searchInProximity(zipcode):
     #---------------------------------------
@@ -44,6 +70,31 @@ def searchWithKeyword(keyword, zipcode):
     # Returns products related to keyword sorted by distance if possible.
     #---------------------------------------
     pass
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/registerform', methods = ['POST'])
+def registerform():
+    newUser = {}
+
+    uname = request.form['uname']
+    pwd = request.form['pwd']
+    zcode = request.form['zcode']
+
+    newUser[uname] = {"pass": pwd, "zcode" = zcode, "products" = []}
+
+    # Put data into DB.
+    if registerUserInDB():
+        # Success
+        userInfo.update
+        with open('userdata.json') as db:
+            userInfo += newUser,
+
+    else:
+        # Failed
+        pass
 
 @app.route('/product/<productID>')
 def getProductInfo(productID):
@@ -60,6 +111,22 @@ def upload():
 
     pass
 
+@app.route('/upload_product_form', methods = ['POST'])
+def upload_product_form():
+    pname = request.form['uname']
+    pdesc = request.form['pdesc']
+    purl = request.form['purl']
+    pyear = request.form['pyear']
+
+
+    # Put data into DB.
+    if registerUserInDB():
+        # Success
+        pass
+    else:
+        # Failed
+        pass
+
 def uploadStatus(message):
     #---------------------------------------
     # Shows the message on a webpage.
@@ -71,6 +138,11 @@ def myproducts():
     #---------------------------------------
     # Shows the current products uploaded by user.
     #---------------------------------------
+
+    # Fetch products from DB
+    products = {}
+
+    # Need only render--------*******************************
     pass
 
 @app.route('/exchange')
@@ -78,6 +150,8 @@ def exchange():
     #---------------------------------------
     # Returns current products of user and selected product to exchange with.
     #---------------------------------------
+
+    # Need only render--------*******************************
     pass
 
 @app.route('/inbox')
@@ -85,6 +159,7 @@ def inbox():
     #---------------------------------------
     # Returns the messages with read or unread status.
     #---------------------------------------
+
     pass
 
 
@@ -100,3 +175,8 @@ if __name__ == "__main__":
 #   Description (String)
 #   Year Bought In (Integer)
 #
+
+# json:
+# {<uname>:
+#   pass: "",
+#   }
