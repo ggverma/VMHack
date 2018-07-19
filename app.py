9cde78865f5d68902263599a57d184909c63db30
Gautam Verma <vermag@vmware.com>
2018-07-19 12:28:12 -0700

    Base works. Yet to integrate UI

--- app.py
+++ app.py
@@ -7,28 +7,29 @@ def index():
     #---------------------------------------
     # If not logged in, user must login first.
     #---------------------------------------
+    # return render_template('index.html', url="http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif")
     if not loggedIn():
-        login()
+``        return login()
     else:
         #---------------------------------------
         # Returns some products
         # If location is available, display local results first.
         #---------------------------------------
-        pass
+        return render_template('index.html', url="http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif")
 
 def loggedIn():
     #---------------------------------------
     # Checks if a user is logged in.
     # Returns true or false
     #---------------------------------------
-    pass
+    return False
 
 @app.route('/login')
 def login():
     #---------------------------------------
     # A web page showing some random products and facebook login
     #---------------------------------------
-    pass
+    return render_template('index.html', url="http://img.buzzfeed.com/buzzfeed-static/static/2013-10/enhanced/webdr05/15/9/anigif_enhanced-buzz-26388-1381844103-11.gif")
 
 def searchInProximity(zipcode):
     #---------------------------------------
@@ -79,25 +80,22 @@ def exchange():
     #---------------------------------------
     pass
 
-@app.route('/inbox'):
+@app.route('/inbox')
 def inbox():
     #---------------------------------------
     # Returns the messages with read or unread status.
     #---------------------------------------
     pass
 
-@app.route('/')
-def index():
-    url = random.choice(images)
-    return render_template('index.html', url=url)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug = True)
 
 ################################################################################
 # Fields in:
 # Product:
 #   ID (Integer)
+#   Image URL (url)
 #   Name (String)
 #   Description (String)
 #   Year Bought In (Integer)
